c7ee60d705e43b18d1008e25587e82e82ab8bb27
AAN3AC <AAN3AC@users.noreply.github.com>
2015-10-03 20:18:31 -0400

    Allow SELINUX to be disabled

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -28,7 +28,7 @@ BOARD_KERNEL_BASE := 0x80200000
 BOARD_KERNEL_PAGESIZE := 2048
 # BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=msm8960 maxcpus=2
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=flo user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3
-BOARD_KERNEL_CMDLINE += vmalloc=340M enforcing=0
+BOARD_KERNEL_CMDLINE += vmalloc=340M androidboot.selinux=permissive/g
 #BOARD_KERNEL_CMDLINE += selinux=0
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000
 
